e2ef04dec943f5655d700ae839dceb42849bb806
DaRabus <dominikrabus@gmail.com>
2020-05-09 17:21:46 +0200

    sauber

--- build-RabusBot-Desktop-Debug/TradingSystem-SCRY-master/generateVSProject.bat
+++
@@ -1,8 +0,0 @@
-::Run this file via VSDevelopmentConsole to generate windows project and build
-
-mkdir build_windows
-cd build_windows
-cmake .. -G "Visual Studio 14 Win64"
-
-msbuild TradingSystem.sln
-copy PoloniexAPI\Debug\PoloniexAPI.dll Debug\
--- build-RabusBot-Desktop-Debug/TradingSystem-SCRY-master/runWindowsDebug.bat
+++
@@ -1,2 +0,0 @@
-cd build_windows\Debug
-TradingSystem.exe
